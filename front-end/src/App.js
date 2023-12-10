bd8e73967a1dd3e930755763e0be509561b25198
sami <mahfoudhisami1207@gmail.com>
2023-12-10 20:37:25 +0100

    profil

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/NavBar&&Footer/Footer";
 import NavBar from "./components/NavBar&&Footer/NavBar";
 import Home from "./components/home";
-import Profile from "./components/profile";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <NavBar />
-        
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
