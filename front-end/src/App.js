ef085ec9ca2d3c6749893ab144d87c8eaac5db41
fahdaroua <fahd.aroua@gmail.com>
2023-12-10 18:59:48 +0100

    someupdates

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css";
 import Footer from "./components/NavBar&&Footer/Footer";
 import NavBar from "./components/NavBar&&Footer/NavBar";
 import Home from "./components/home";
+import Profile from "./components/profile";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    < Home/>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
