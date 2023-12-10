658a88d757a85600aef90559d861620734fe8475
sami <mahfoudhisami1207@gmail.com>
2023-12-10 16:23:44 +0100

    navbar&&footer

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import Footer from "./components/NavBar&&Footer/Footer";
+import NavBar from "./components/NavBar&&Footer/NavBar";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
