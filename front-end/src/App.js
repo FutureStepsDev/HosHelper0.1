900af3f50d5add575904034a055d43e9f128cba7
Wifek Benkhoud <wifekbenkhoud@gmail.com>
2023-12-11 20:08:37 +0100

    test

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
 import Footer from "./components/NavBar&&Footer/Footer";
 import NavBar from "./components/NavBar&&Footer/NavBar";
-<<<<<<< HEAD
 import Home from "./components/home";
 import Profile from "./components/profile";
 import Login from './components/Login';
 import SignUpForm from './components/Signup' 
 
-=======
-import Home from "./components/home"
->>>>>>> parent of 9be4ffe (ading path)
 function App() {
   return (
     <div className="App">
