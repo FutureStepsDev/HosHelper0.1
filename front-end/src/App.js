be1708fc7aa51718f473f11d5ee73df849fae9b6
Wifek Benkhoud <wifekbenkhoud@gmail.com>
2023-12-11 19:36:54 +0100

    test

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,27 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css";
 import Footer from "./components/NavBar&&Footer/Footer";
 import NavBar from "./components/NavBar&&Footer/NavBar";
+<<<<<<< HEAD
 import Home from "./components/home";
 import Profile from "./components/profile";
 import Login from './components/Login';
 import SignUpForm from './components/Signup' 
 
+=======
+import Home from "./components/home"
+>>>>>>> parent of 9be4ffe (ading path)
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <NavBar />
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <NavBar />
+      <Home/>
+      <Footer />
+    </div>
+   
   );
 }
 
