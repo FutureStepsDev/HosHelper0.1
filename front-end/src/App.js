6d60575046acc140bb000933cad20a61cbd04d66
fahdaroua <fahd.aroua@gmail.com>
2023-12-12 21:03:56 +0100

    pharmacy

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,8 @@ import Footer from "./components/NavBar&&Footer/Footer";
 import NavBar from "./components/NavBar&&Footer/NavBar";
 import Home from "./components/home";
 import Profile from "./components/profile";
+import Hospital from './components/pages/Hospital';
+import Pharmacy from './components/pages/Pharmacy';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/hospital" element={<Hospital />} />
+          <Route path="/pharmacy" element={<Pharmacy />} />
         </Routes>
 
         <Footer />
