f44c41785f3542b5b0d4d946f9d21666ea3b45c9
Wifek Benkhoud <wifekbenkhoud@gmail.com>
2023-12-18 20:20:35 +0100

    worcking on the map cart

--- front-end/src/components/pages/Map.js
+++ front-end/src/components/pages/Map.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useRef } from 'react';
 import 'ol/ol.css';
+import 'ol-geocoder/dist/ol-geocoder.min.css';
 import { Map as OlMap, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import Geocoder from 'ol-geocoder';
 
 const Map = ({ address }) => {
-  const mapRef = useRef(null);
+ const mapRef = useRef(null);
 
-  useEffect(() => {
+ useEffect(() => {
     // Initialize the map
     const map = new OlMap({
       layers: [
@@ -24,22 +30,35 @@ const Map = ({ address }) => {
     // Set the map target to the div element
     map.setTarget(mapRef.current);
 
-    // Fetch coordinates based on the address using a geocoding service
-    // (You need to replace this with your geocoding logic)
-    // For example, you can use the OpenLayers Geocoding API or any other service
-    // to convert the address to coordinates and update the map view.
+    // Initialize the vector source and layer
+    const vectorSource = new VectorSource();
+    const vectorLayer = new VectorLayer({
+      source: vectorSource,
+    });
+    map.addLayer(vectorLayer);
 
-    // For now, let's set a default location
-    map.getView().setCenter([0, 0]);
-    map.getView().setZoom(2);
+    // Add a geocoder control to the map
+    const geocoder = new Geocoder('nominatim', {
+      targetType: 'glass-button',
+      lang: 'en-US',
+      placeholder: 'Search for ...',
+      limit: 5,
+    });
+    map.addControl(geocoder);
+
+    // Add an event listener to the geocoder control to update the map view when a user selects a location
+    geocoder.on('select', (e) => {
+      map.getView().setCenter(e.coordinate);
+      map.getView().setZoom(12);
+    });
 
     // Clean up the map instance when the component is unmounted
     return () => {
       map.setTarget(undefined);
     };
-  }, [address]);
+ }, [address]);
 
-  return <div ref={mapRef} style={{ width: '100%', height: '150px', borderRadius: '8px', marginTop: '8px' }} />;
+ return <div ref={mapRef} style={{ width: '100%', height: '150px', borderRadius: '8px', marginTop: '8px' }} />;
 };
 
 export default Map;
