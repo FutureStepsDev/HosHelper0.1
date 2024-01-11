7fc1f98b8c60e7c9d9b0a50597e5eb42205589d0
fahdaroua <fahd.aroua@gmail.com>
2024-01-11 19:59:23 +0100

    footer

--- front-end/src/components/NavBar&&Footer/Footer.js
+++ front-end/src/components/NavBar&&Footer/Footer.js
@@ -10,22 +10,20 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   footer: {
     backgroundColor: "rgb(255, 255, 255)",
-    padding: theme.spacing(6),
-    marginTop: "auto",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    borderTop: "1px solid #ccc", // Border at the top of the footer
-  },
-  aboutUsContainer: {
+    padding: theme.spacing(3),
+    marginTop: "120px",
+    borderTop: "1px solid #ccc",
+    height: "60px",
+    width: "100%",
     display: "flex",
     justifyContent: "space-around",
-    width: "100%",
+    alignItems: "center",
   },
   aboutUsColumn: {
-    flex: 1,
-    padding: theme.spacing(2),
     textAlign: "center",
+  },
+  contactUs: {
+    minWidth: "200px",
   },
   icon: {
     marginRight: theme.spacing(1),
@@ -37,35 +35,32 @@ const Footer = () => {
 
   return (
     <footer className={classes.footer}>
-      <div className={classes.aboutUsContainer}>
-        <div className={classes.aboutUsColumn}>
-          <Typography variant="h6">About Us</Typography>
-          <Typography variant="body2">This site is to help you to make your appointement more easy</Typography>
-        </div>
-        <div className={classes.aboutUsColumn}>
-          <Typography variant="h6">Contact Us</Typography>
-          <Typography variant="body2">
-            <IconButton className={classes.icon}>
-              <MailIcon />
-            </IconButton>
-           Hoshelper@gmail.com
-          </Typography>
-          <Typography variant="body2">
-            <IconButton className={classes.icon}>
-              <PhoneIcon />
-            </IconButton>
-            +216 21 644 554
-          </Typography>
-        </div>
-        <div className={classes.aboutUsColumn}>
-          <Typography variant="h6">Social Media</Typography>
+      <div className={`${classes.aboutUsColumn} ${classes.contactUs}`}>
+        <Typography variant="h7">© Copyright 2024 Hoshelper Tous droits réservés</Typography>
+      </div>
+      <div className={`${classes.aboutUsColumn} ${classes.contactUs}`}>
+        <Typography variant="body2">Contact Us</Typography>
+        <Typography variant="h7">
           <IconButton className={classes.icon}>
-            <InstagramIcon />
+            <MailIcon />
           </IconButton>
+          Hoshelper@gmail.com
+        </Typography>
+        <Typography variant="h7">
           <IconButton className={classes.icon}>
-            <FacebookIcon />
+            <PhoneIcon />
           </IconButton>
-        </div>
+          +216 21 644 554
+        </Typography>
+      </div>
+      <div className={classes.aboutUsColumn}>
+        <Typography variant="body2">Social Media</Typography>
+        <IconButton className={classes.icon}>
+          <InstagramIcon />
+        </IconButton>
+        <IconButton className={classes.icon}>
+          <FacebookIcon />
+        </IconButton>
       </div>
     </footer>
   );
