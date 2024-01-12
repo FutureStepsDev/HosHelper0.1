b2921b3f92a8d2585ae8878e5e287f660abb0db1
sami <mahfoudhisami1207@gmail.com>
2024-01-12 18:24:31 +0100

    last-push

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -47,7 +47,7 @@ module.exports = (sequelize) => {
         type: DataTypes.STRING,
         allowNull: true,
       },
-      Height: {
+      tel: {
         type: DataTypes.STRING,
         allowNull: true,
       },
